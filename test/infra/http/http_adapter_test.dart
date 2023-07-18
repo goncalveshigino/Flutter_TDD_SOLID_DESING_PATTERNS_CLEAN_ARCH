18f444067826c45827f9f1fae0a8a8f055c6e923
goncalveshigino <goncalveshigino20@gmail.com>
2023-07-18 20:52:36 +0100

    test:ensure HttpAdater returns null if post returns 204 with data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -24,9 +24,14 @@ class HttpAdapter implements HttpClient {
       'accept': 'application/json',
     };
     final jsonBody = body != null ? jsonEncode(body) : null;
-    final response =
-        await client.post(Uri.parse(url), headers: headers, body: jsonBody);
-    return response.body.isEmpty ? null : jsonDecode(response.body);
+    final response = await client.post(Uri.parse(url), headers: headers, body: jsonBody);
+
+    if ( response.statusCode == 200 ) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else {
+      return null;
+    }
+    
   }
 }
 
@@ -93,6 +98,14 @@ void main() {
 
     test('Should return null if post returns 204', () async {
       mockResponse(204, body: '');
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
+
+
+    test('Should return null if post returns 204 with data', () async {
+      mockResponse(204);
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, null);
