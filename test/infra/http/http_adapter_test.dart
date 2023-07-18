c8b3fab2b606c087cd16bfa9db1d961f7a30b169
goncalveshigino <goncalveshigino20@gmail.com>
2023-07-18 19:20:45 +0100

    test:ensure HttpAdapter calls post with correct Headers

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -13,7 +13,11 @@ class HttpAdapter {
     @required String url,
     @required String method,
   }) async {
-    await client.post(Uri.parse(url));
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
+    await client.post(Uri.parse(url), headers: headers);
   }
 }
 
@@ -23,13 +27,20 @@ void main() {
   group('post', () {});
 
   test('Should calls post with correct values', () async {
-    
     final client = ClientSpay();
     final sut = HttpAdapter(client);
     final url = faker.internet.httpUrl();
 
     await sut.request(url: url, method: 'post');
 
-    verify(client.post(Uri.parse(url)));
+    verify(
+      client.post(
+        Uri.parse(url),
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+        },
+      ),
+    );
   });
 }
