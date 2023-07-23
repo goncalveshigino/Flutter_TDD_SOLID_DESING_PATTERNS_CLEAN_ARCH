e1653b244a6d51e59e46e878bbd2aaf074966146
goncalveshigino <goncalveshigino20@gmail.com>
2023-07-23 13:36:45 +0100

    test:ensure IsFormValid emits null if email is valid

--- test/presentation/presenters/streams_login_presenter_test.dart
+++ test/presentation/presenters/streams_login_presenter_test.dart
@@ -50,4 +50,14 @@ void main() {
   });
 
 
+  test('Should emit null if validation succeeds', () {  
+     sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+     sut.isFormValidStream.listen(expectAsync1((isValid) => expect(isValid, false)));
+
+
+     sut.validateEmail(email);
+     sut.validateEmail(email);
+  });
+
+
 }
