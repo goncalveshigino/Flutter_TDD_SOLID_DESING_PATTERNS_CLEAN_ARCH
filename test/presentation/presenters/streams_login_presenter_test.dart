35487d143f889da9ca720426d512319aab511ba3
goncalveshigino <goncalveshigino20@gmail.com>
2023-07-23 12:01:55 +0100

    test:ensure StreamLoginPresenter calls validations on email changed

---
+++ test/presentation/presenters/streams_login_presenter_test.dart
@@ -0,0 +1,46 @@
+import 'package:faker/faker.dart';
+import 'package:flutter/material.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+// Validar Estream
+
+abstract class Validation {
+  String validate({@required String field, @required String value});
+}
+
+class StreamLoginPresenter {
+
+  final Validation validation;
+  StreamLoginPresenter({ @required this.validation});
+
+  void validateEmail(String email) {
+     validation.validate(field: 'email', value: email);
+  }
+}
+
+
+
+class ValidationSpy extends Mock implements Validation {}
+
+
+void main() {
+
+  StreamLoginPresenter sut;
+  ValidationSpy validation;
+  String email;
+
+  setUp((){
+     validation = ValidationSpy();
+     sut = StreamLoginPresenter(validation: validation);
+     email = faker.internet.email();
+  });
+
+  test('Should call Validation with correct email', () {
+    sut.validateEmail(email);
+
+    verify(validation.validate(field: 'email', value: email)).called(1);
+  });
+
+  
+}
