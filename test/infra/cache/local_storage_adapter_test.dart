0806c31acbc41471a24ea770669f7c3d5190cc3e
goncalveshigino <goncalveshigino20@gmail.com>
2023-07-27 12:37:00 +0100

    test:ensure LocalStorageAdapter throws if save secure throws

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -1,21 +1,13 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:meta/meta.dart';
+
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-import 'package:flutter_tdd_clean_arch_solid_desin_patterns/data/cache/cache.dart';
 
-class LocalStorageAdapter implements SaveSecureCacheStorage {
-  final FlutterSecureStorage secureStorage;
+import 'package:flutter_tdd_clean_arch_solid_desin_patterns/infra/cache/cache.dart';
 
-  LocalStorageAdapter({@required this.secureStorage});
-  @override
-  Future<void> saveSecure(
-      {@required String key, @required String value}) async {
-    await secureStorage.write(key: key, value: value);
-  }
-}
+
 
 class FlutterSecuteStorageSpy extends Mock implements FlutterSecureStorage {}
 
@@ -26,16 +18,28 @@ void main() {
   String value;
 
   setUp(() {
-     secureStorage = FlutterSecuteStorageSpy();
-     sut = LocalStorageAdapter(secureStorage: secureStorage);
-     key = faker.lorem.word();
-     value = faker.guid.guid();
+    secureStorage = FlutterSecuteStorageSpy();
+    sut = LocalStorageAdapter(secureStorage: secureStorage);
+    key = faker.lorem.word();
+    value = faker.guid.guid();
   });
+
+  void mockSaveSecureError() {
+    when(secureStorage.write(key: anyNamed('key'), value: anyNamed('value')))
+        .thenThrow(Exception());
+  }
 
   test('Should call save secure with correct values', () async {
     await sut.saveSecure(key: key, value: value);
 
     verify(secureStorage.write(key: key, value: value));
   });
-  
+
+  test('Should throws if save secure throws', () async {
+    mockSaveSecureError();
+
+    final future = sut.saveSecure(key: key, value: value);
+
+    expect(future, throwsA(TypeMatcher<Exception>()));
+  });
 }
