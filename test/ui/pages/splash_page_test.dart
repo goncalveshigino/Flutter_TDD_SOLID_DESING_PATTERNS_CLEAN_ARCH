a5c1f9604db864bcd3b9cc3dcee639666b3c7b48
goncalveshigino <goncalveshigino20@gmail.com>
2023-07-28 12:59:28 +0100

    test:ensure splashPage navigate to correct page

--- test/ui/pages/splash_page_test.dart
+++ test/ui/pages/splash_page_test.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:mockito/mockito.dart';
@@ -8,40 +9,59 @@ class SplashPage extends StatelessWidget {
   final SplashPresenter presenter;
 
   const SplashPage({Key key, @required this.presenter}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     presenter.loadCurrentAccount();
-    
+
     return Scaffold(
       appBar: AppBar(title: const Text('4Dev')),
-      body: const Center(
-        child: CircularProgressIndicator(),
-      ),
+      body: Builder(builder: (context) {
+        presenter.navigateToStream.listen((page) {
+          if (page?.isNotEmpty == true) {
+            Get.offAllNamed(page);
+          }
+        });
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }),
     );
   }
 }
 
 abstract class SplashPresenter {
+  Stream<String> get navigateToStream;
+
   Future<void> loadCurrentAccount();
 }
 
 class SplasePresenterSpy extends Mock implements SplashPresenter {}
 
 void main() {
-
   SplasePresenterSpy presenter;
-
+  StreamController<String> navigateToController;
 
   Future<void> loadPage(WidgetTester tester) async {
     presenter = SplasePresenterSpy();
+    navigateToController = StreamController<String>();
+    when(presenter.navigateToStream)
+        .thenAnswer((_) => navigateToController.stream);
+
     await tester.pumpWidget(GetMaterialApp(
       initialRoute: '/',
       getPages: [
-        GetPage(name: '/', page: () => SplashPage(presenter: presenter))
+        GetPage(name: '/', page: () => SplashPage(presenter: presenter)),
+        GetPage(
+            name: '/any_route',
+            page: () => const Scaffold(body: Text('fake page')))
       ],
     ));
   }
+
+  tearDown(() {
+    navigateToController.close();
+  });
 
   testWidgets('Should present spinner on page load',
       (WidgetTester tester) async {
@@ -50,9 +70,20 @@ void main() {
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
   });
 
-  testWidgets('Should call loadCurrentAccount on page load',(WidgetTester tester) async {
+  testWidgets('Should call loadCurrentAccount on page load',
+      (WidgetTester tester) async {
     await loadPage(tester);
 
     verify(presenter.loadCurrentAccount()).called(1);
+  });
+
+  testWidgets('Should load page', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    navigateToController.add('/any_route');
+    await tester.pumpAndSettle();
+
+    expect(Get.currentRoute, '/any_route');
+    expect(find.text('fake page'), findsWidgets);
   });
 }
