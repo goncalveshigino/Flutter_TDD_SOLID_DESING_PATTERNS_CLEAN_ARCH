1ca7f3fd677ed8d25bdbf343c855af0041290198
goncalveshigino <goncalveshigino20@gmail.com>
2023-08-09 06:01:16 +0100

    test:ensure remoteAddAcount throws UnexpectedError on 500

--- test/data/usecases/addacount/remote_addacount_test.dart
+++ test/data/usecases/addacount/remote_addacount_test.dart
@@ -80,4 +80,13 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+
+  test('Should throw UnexpectedError if HttpClient returns 500', () async {
+    mockHttpError(HttpError.serverError);
+
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
