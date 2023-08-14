1d2a1ba08338a0323a443eb170493c444f6eb92e
goncalveshigino <goncalveshigino20@gmail.com>
2023-08-14 09:21:32 +0100

    fix:mostrar erro caso o email ja estiver a ser utilizado

--- test/presentation/presenters/getx_signup_presenter_test.dart
+++ test/presentation/presenters/getx_signup_presenter_test.dart
@@ -46,6 +46,10 @@ void main() {
     mockSaveCurrentAccountCall().thenThrow(DomainError.unexpected);
   }
 
+   void mockAddAccountError(DomainError error) {
+    mockAddAccountCall().thenThrow(error);
+  }
+
 
   setUp(() {
     validation = ValidationSpy();
@@ -293,6 +297,35 @@ void main() {
 
     expectLater(sut.isLoadingStream, emits(true));
     
+
+    await sut.signUp();
+  });
+
+
+
+  
+   test('Should emit correct events on EmailInUseError', () async {
+    mockAddAccountError(DomainError.emailInUse);
+    sut.validateName(name);
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+    sut.validatePasswordConfirmation(passwordConfirmation);
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+    sut.mainErrorStream.listen( expectAsync1((error) => expect(error, UIError.emailInUse)));
+
+    await sut.signUp();
+  });
+
+  test('Should emit correct events on UnexpectedError', () async {
+    mockAddAccountError(DomainError.unexpected);
+    sut.validateName(name);
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+    sut.validatePasswordConfirmation(passwordConfirmation);
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+    sut.mainErrorStream.listen(expectAsync1((error) => expect(error, UIError.unexpected)));
 
     await sut.signUp();
   });
