ed46555e0695e8e90e337fd81fe7bb12679f9fee
goncalveshigino <goncalveshigino20@gmail.com>
2023-08-17 20:52:34 +0100

    test:ensure HttpAdapter returns null on 204

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -162,6 +162,7 @@ void main() {
   });
 
   group('get', () {
+    
     PostExpectation mockRequest() => when(client.get(any, headers: anyNamed('headers')));
 
     void mockResponse(int statusCode,
@@ -203,5 +204,21 @@ void main() {
 
       expect(response, null);
     });
+
+
+     test('Should return null if get returns 204', () async {
+      mockResponse(204, body: '');
+      final response = await sut.request(url: url, method: 'get');
+
+      expect(response, null);
+    });
+
+    test('Should return null if get returns 204 with data', () async {
+      mockResponse(204);
+      final response = await sut.request(url: url, method: 'get');
+
+      expect(response, null);
+    });
+
   });
 }
