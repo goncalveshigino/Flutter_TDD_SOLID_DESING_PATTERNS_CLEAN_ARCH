68ba1e09dbde54a9346db8b70e1cb811f3d2de16
goncalveshigino <goncalveshigino20@gmail.com>
2023-08-22 08:23:06 +0100

    feat:move GetxSurveysPresenter to its own file

---
+++ lib/presentation/presenter/getx_surveys_presenter.dart
@@ -0,0 +1,38 @@
+import 'package:intl/intl.dart';
+import 'package:meta/meta.dart';
+import 'package:get/get.dart';
+
+import '../../domain/domain.dart';
+import '../../ui/ui.dart';
+
+class GetxSurveysPresenter {
+  
+  final LoadSurveys loadSurveys;
+
+  final _isLoading = true.obs;
+  final _surveys = Rx<List<SurveyViewModel>>();
+
+  Stream<bool> get isLoadingStream => _isLoading.stream;
+  Stream<List<SurveyViewModel>> get surveysStream => _surveys.stream;
+
+  GetxSurveysPresenter({@required this.loadSurveys});
+
+  Future<void> loadData() async {
+    try {
+      _isLoading.value = true;
+      final surveys = await loadSurveys.load();
+      _surveys.value = surveys
+          .map((survey) => SurveyViewModel(
+                id: survey.id,
+                question: survey.question,
+                date: DateFormat('dd MMM yyyy').format(survey.dateTime),
+                didAnswer: survey.didAnswer,
+              ))
+          .toList();
+    } on DomainError {
+      _surveys.subject.addError(UIError.unexpected.description);
+    } finally {
+      _isLoading.value = false;
+    }
+  }
+}
--- test/presentation/presenters/getx_surveys_presenter_test.dart
+++ test/presentation/presenters/getx_surveys_presenter_test.dart
@@ -1,46 +1,19 @@
 import 'package:faker/faker.dart';
-import 'package:flutter_tdd_clean_arch_solid_desin_patterns/domain/helpers/helpers.dart';
-import 'package:get/get.dart';
-import 'package:meta/meta.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:intl/intl.dart';
+
 
 import 'package:flutter_tdd_clean_arch_solid_desin_patterns/domain/usecases/usecases.dart';
 import 'package:flutter_tdd_clean_arch_solid_desin_patterns/domain/entities/entities.dart';
+import 'package:flutter_tdd_clean_arch_solid_desin_patterns/domain/helpers/helpers.dart';
+
 
 import 'package:flutter_tdd_clean_arch_solid_desin_patterns/ui/ui.dart';
 
-class GetxSurveysPresenter {
-  final LoadSurveys loadSurveys;
 
-  final _isLoading = true.obs;
-  final _surveys = Rx<List<SurveyViewModel>>();
+import 'package:flutter_tdd_clean_arch_solid_desin_patterns/presentation/presenter/presenter.dart';
 
-  Stream<bool> get isLoadingStream => _isLoading.stream;
-  Stream<List<SurveyViewModel>> get surveysStream => _surveys.stream;
 
-  GetxSurveysPresenter({@required this.loadSurveys});
-
-  Future<void> loadData() async {
-    try {
-      _isLoading.value = true;
-      final surveys = await loadSurveys.load();
-      _surveys.value = surveys
-          .map((survey) => SurveyViewModel(
-                id: survey.id,
-                question: survey.question,
-                date: DateFormat('dd MMM yyyy').format(survey.dateTime),
-                didAnswer: survey.didAnswer,
-              ))
-          .toList();
-    } on DomainError {
-      _surveys.subject.addError(UIError.unexpected.description);
-    } finally {
-      _isLoading.value = false;
-    }
-  }
-}
 
 class LoadSurveysSpy extends Mock implements LoadSurveys {}
 
