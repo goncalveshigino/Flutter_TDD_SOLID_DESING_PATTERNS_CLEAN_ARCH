cbaa14644df2d154b88b77748f292dede31b726a
goncalveshigino <goncalveshigino20@gmail.com>
2023-08-25 08:31:57 +0100

    feat:move SUT creation to the setup method

--- test/main/decorators/authorize_http_client_decorator_test.dart
+++ test/main/decorators/authorize_http_client_decorator_test.dart
@@ -4,26 +4,36 @@ import 'package:test/test.dart';
 
 import 'package:flutter_tdd_clean_arch_solid_desin_patterns/data/data.dart';
 
-
 class AuthorizeHttpClientDecorator {
   final FetchSecureCacheStorage fetchSecureCacheStorage;
 
   AuthorizeHttpClientDecorator({@required this.fetchSecureCacheStorage});
-  
+
   Future<void> request() async {
-     await fetchSecureCacheStorage.fetchSecure('token');
+    await fetchSecureCacheStorage.fetchSecure('token');
   }
 }
 
-class FetchSecureCacheStorageSpy extends Mock implements FetchSecureCacheStorage {}
+class FetchSecureCacheStorageSpy extends Mock
+    implements FetchSecureCacheStorage {}
+
 void main() {
+  
+  AuthorizeHttpClientDecorator sut;
+  FetchSecureCacheStorageSpy fetchSecureCacheStorage;
 
-  test('Should call FetchSecureCacheStorage with correct key',() async {
-     final fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
-     final sut = AuthorizeHttpClientDecorator(fetchSecureCacheStorage: fetchSecureCacheStorage);
+  setUp(() {
+    fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
+     sut = AuthorizeHttpClientDecorator(fetchSecureCacheStorage: fetchSecureCacheStorage);
+  });
 
-     await sut.request();
+  test('Should call FetchSecureCacheStorage with correct key', () async {
+    final fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
+    final sut = AuthorizeHttpClientDecorator(
+        fetchSecureCacheStorage: fetchSecureCacheStorage);
 
-     verify(fetchSecureCacheStorage.fetchSecure('token')).called(1);
+    await sut.request();
+
+    verify(fetchSecureCacheStorage.fetchSecure('token')).called(1);
   });
 }
