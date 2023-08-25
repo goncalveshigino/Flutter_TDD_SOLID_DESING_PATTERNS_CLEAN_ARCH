5715056eee2b9c7a145885e66f78e474a3dee5ff
goncalveshigino <goncalveshigino20@gmail.com>
2023-08-25 09:06:28 +0100

    testensure decorator append  header if header if not null

--- test/main/decorators/authorize_http_client_decorator_test.dart
+++ test/main/decorators/authorize_http_client_decorator_test.dart
@@ -23,7 +23,7 @@ class AuthorizeHttpClientDecorator {
   }) async {
 
     final token = await fetchSecureCacheStorage.fetchSecure('token');
-    final authorizedHeaders = {'x-access-token': token};
+    final authorizedHeaders = headers ?? {}..addAll({'x-access-token': token});
     await decoratee.request(url: url, method: method, body: body, headers: authorizedHeaders);
 
   }
@@ -34,6 +34,7 @@ class FetchSecureCacheStorageSpy extends Mock implements FetchSecureCacheStorage
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
+  
   AuthorizeHttpClientDecorator sut;
   FetchSecureCacheStorageSpy fetchSecureCacheStorage;
   HttpClientSpy httpClient;
@@ -69,11 +70,10 @@ void main() {
 
   test('Should call decoratee with access token on header', () async { 
     await sut.request(url: url, method: method, body: body);
+    verify(httpClient.request(url: url,method: method,body: body,headers: {'x-access-token': token})).called(1);
 
-    verify(httpClient.request(
-        url: url,
-        method: method,
-        body: body,
-        headers: {'x-access-token': token})).called(1);
+
+    await sut.request(url: url, method: method, body: body, headers:  {'any_header': 'any_value'});
+    verify(httpClient.request(url: url,method: method,body: body,headers: {'x-access-token': token, 'any_header': 'any_value'})).called(1);
   });
 }
